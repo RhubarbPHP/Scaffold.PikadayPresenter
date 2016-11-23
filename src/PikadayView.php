53854c4aa5ed496f6f268a8b733d338c11e7cab3
robmadeyou <robmadeyou@gmail.com>
2016-11-23 11:52:46 +0000

    Formatted the date time on save

--- src/PikadayView.php
+++ src/PikadayView.php
@@ -2,22 +2,25 @@
 
 namespace Rhubarb\Pikaday;
 
-use Rhubarb\Leaf\Controls\Common\Text\TextBoxView;
+use Rhubarb\Crown\DateTime\RhubarbDate;
+use Rhubarb\Crown\Request\WebRequest;
+use Rhubarb\Leaf\Leaves\Controls\ControlView;
 use Rhubarb\Leaf\Leaves\LeafDeploymentPackage;
 
 /**
  * @property PikadayModel $model
  */
-class PikadayView extends TextBoxView
+class PikadayView extends ControlView
 {
-    protected $requiresStateInput = true;
-
     public function printViewContent()
     {
         switch ($this->model->mode) {
             case PikadayModel::MODE_TEXT_INPUT:
-                parent::printViewContent();
+                print '<input type="text" leaf-name="' . $this->model->leafName . '" name="' . $this->model->leafPath . '" ' .
+                    'id="' . $this->model->leafPath . '" ' . $this->model->getClassAttribute() . $this->model->getHtmlAttributes() .
+                    ' value="' . $this->getFormattedValue() . '" />';
                 return;
+
             case PikadayModel::MODE_CALENDAR:
                 print '<div id="' . htmlentities($this->model->leafPath) . '" ' .
                     'leaf-name="' . htmlentities($this->model->leafName) . '"' .
@@ -41,7 +44,7 @@ class PikadayView extends TextBoxView
         $value = $this->model->value;
 
         if ($value instanceof \DateTime) {
-            $value = $value->format('d/m/Y');
+            $value = $value->format($this->model->dateFormat);
         }
 
         return htmlentities($value);
@@ -54,8 +57,7 @@ class PikadayView extends TextBoxView
 
     public function getDeploymentPackage()
     {
-        /** @var LeafDeploymentPackage $package */
-        $package = parent::getDeploymentPackage();
+        $package = new LeafDeploymentPackage();
 
         $package->resourcesToDeploy[] = __DIR__ . '/../../../../components/moment/min/moment.min.js';
         $package->resourcesToDeploy[] = __DIR__ . '/../../../rhubarbphp/pikaday/pikaday.js';
@@ -66,5 +68,31 @@ class PikadayView extends TextBoxView
         }
 
         return $package;
+    }
+
+    protected function parseRequest(WebRequest $request)
+    {
+        $path = $this->model->leafPath;
+
+        // By default if a control can be represented by a single HTML element then the name of that element
+        // should equal the leaf path of the control. If that is true then we can automatically discover and
+        // update our model.
+
+        $value = $request->post($path);
+        if ($value !== null) {
+            $this->model->setValue(RhubarbDate::createFromFormat($this->model->dateFormat, $value));
+        }
+
+        // Now we search for indexed data. We can't unfortunately guess what the possible indexes are so we
+        // have to use a regular expression to find and extract any indexes. Note that it's not possible to
+        // have both un-indexed and indexed versions of the same leaf on the parent. In that case the indexed
+        // version will create an array of model data in place of the single un-indexed value.
+        $postData = $request->postData;
+
+        foreach ($postData as $key => $value) {
+            if (preg_match('/' . preg_quote($this->model->leafPath) . '\(([^)]+)\)$/', $key, $match)) {
+                $this->setControlValueForIndex($match[1], RhubarbDate::createFromFormat($this->model->dateFormat, $value));
+            }
+        }
     }
 }
